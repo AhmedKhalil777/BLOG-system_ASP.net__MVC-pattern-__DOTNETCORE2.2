b3a20811232b8c93697957fe750d3ead133074f7
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2019-07-28 22:05:03 +0200

    initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BLOG_system_ASP.net__MVC_pattern___DOTNETCORE2._2.Models;
+
+namespace BLOG_system_ASP.net__MVC_pattern___DOTNETCORE2._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
